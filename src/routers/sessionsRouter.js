75317e6ce514442512ddde712224c6af9429dce5
VodeniZeko <saletovic.developer@gmail.com>
2024-04-10 00:27:18 -0500

    datbase part 2

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const debug = require("debug")("app:sessionsRouter");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const {username, password, host, database} = require('../../config');
 
 const sessionsRouter = express.Router();
@@ -28,9 +28,27 @@ sessionsRouter.route("/").get((req, res) => {
 
 sessionsRouter.route("/:id").get((req, res) => {
   const id = req.params.id;
-  res.render("session", {
-    session: sessions[id],
-  });
+
+  const url = `mongodb+srv://${username}:${password}@${host}/?retryWrites=true&w=majority&appName=${database}`;
+  const dbName = `${database}`;
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug("Connected to the Mongo DB");
+
+      const db = client.db(dbName);
+
+      const session = await db.collection("sessions").findOne({ _id: new ObjectId(id) });
+      res.render("session", { session });
+
+    } catch (err) {
+      debug("Could not connect to the server", err.stack);
+      res.status(500).send("Internal Server Error");
+    }
+    client.close();
+  })();
 });
 
 module.exports = sessionsRouter;
