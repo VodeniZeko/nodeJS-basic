3635852a8367ce669390270cdad056ff039f213e
VodeniZeko <saletovic.developer@gmail.com>
2024-04-10 00:00:26 -0500

    pulling data from the mongodb

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,18 +1,36 @@
 const express = require("express");
-const sessions = require("../data/sessions.json")
+const debug = require("debug")("app:sessionsRouter");
+const { MongoClient } = require("mongodb");
+const {username, password, host, database} = require('../../config');
 
 const sessionsRouter = express.Router();
 
-sessionsRouter.route('/').get((req,res) => {
-    res.render("sessions", {
-      sessions,
-    },)})
+sessionsRouter.route("/").get((req, res) => {
+  const url = `mongodb+srv://${username}:${password}@${host}/?retryWrites=true&w=majority&appName=${database}`;
+  const dbName = `${database}`;
 
-sessionsRouter.route('/:id').get((req,res) => {
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug("Connected to the Mongo DB");
+
+      const db = client.db(dbName);
+
+      const sessions = await db.collection("sessions").find().toArray();
+      res.render("sessions", { sessions });
+    } catch (err) {
+      debug("Could not connect to the server", err.stack);
+    }
+    client.close();
+  })();
+});
+
+sessionsRouter.route("/:id").get((req, res) => {
   const id = req.params.id;
-    res.render("session", {
-      session: sessions[id],
-    })
-})
+  res.render("session", {
+    session: sessions[id],
+  });
+});
 
 module.exports = sessionsRouter;
